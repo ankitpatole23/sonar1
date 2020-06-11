93ac5c38b18733c9d89f9516e55fc6c926e1db37
Ankit <Ankit@Dell>
2020-06-11 16:40:19 +0530

    First Commit

---
+++ sonar1/src/main/java/com/microservices/Sonar1Application.java
@@ -0,0 +1,13 @@
+package com.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sonar1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sonar1Application.class, args);
+	}
+
+}
